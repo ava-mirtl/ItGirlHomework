5bc5d6d8a196a6c0f424558ca9c0bd21941411e6
ava-mirtl <ava_mirtl@mail.ru>
2022-06-25 13:10:22 +0300

    marvel

--- 20/marvel.js
+++ 20/marvel.js
@@ -1,2 +1,9 @@
-let massive = JSON.parse(a);
-console.log(massive.halk);
+let ironman, captain, blackwidow, deadpool, spiderman, tor, halk
+fetch('https://raw.githubusercontent.com/ava-mirtl/ItGirlHomework/main/20/marvel.json')
+.then((response => response.json()))
+.then(hero => {ironman = hero[0]; captain = hero[1]; spiderman = hero[2]; halk = hero[3]; tor = hero[4]; blackwidow = hero[5]; deadpool = hero[6]})
+.catch(err => console.log(err));
+
+setTimeout(() => {
+    console.log(ironman, captain, blackwidow, deadpool, spiderman, tor, halk);
+}, 5000);

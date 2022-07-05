2668d6bade17b4426e81a40c93edfbb9c3f435a5
ava-mirtl <ava_mirtl@mail.ru>
2022-07-05 22:02:49 +0300

    gf

--- 21/gif.js
+++ 21/gif.js
@@ -1,44 +1,32 @@
 let btn = document.querySelector('#searchbtn');
 let result = document.querySelector('#responce');
-let src;
-let arr = [];
-
+let array = [];
 
 
 
 btn.addEventListener('click', (e)=>{
+  result.innerHTML="";
+array = [];
     e.preventDefault();   
     async function fetchToDo () {
       let input = document.querySelector('.search'),
       n=input.value
       const url = `https://api.giphy.com/v1/gifs/search?api_key=EwOmWGb4UqWf79KsjbfzikWI8KBiO7WS&limit=5&offset=0&q=${n}`;  
-      const responce = await fetch(url, {
-        headers:{
-          "Content-Type": "application/json",
-          "Accept": "application/json"
-        }
-      })
-      .catch(error => console.log(error))
-      data = await responce.json()
+      const responce = await fetch(url)
+      try {
+        data = await responce.json()
       input.value = ''
       return data
+      } catch (error) {
+        console.log(error)
+      }
     }
     async function getImg(){
       const data = await fetchToDo()
       array = data.data
       array.forEach(el => {
-        arr.push(el.images.downsized.url)
+        result.innerHTML+= `<img src="${el.images.downsized.url}" alt = "">`;
       });
    }
 
-async function viewImg() {
- await  getImg()
- console.log(arr); 
-
- arr.forEach(el =>{
-  let box = document.createElement("img");
-  box.src = el;
-  result.append(box);
-})}
-
-viewImg()})
+getImg()})
